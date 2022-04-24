a5722f1823e4380d3f72023f5603ea723e6d2a25
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-24 22:55:54 +0300

    ft:added a save function that savers users into the user list

--- user.py
+++ user.py
@@ -2,16 +2,22 @@ from user import user
 
 class User:
     """
-    Class that generates new instances of contacts.
+    this generates new  users.
     """
 
-    user_list = [] # Empty contact list
+    user_list = [] # Empty user list
 
     def __init__(self,first_name,last_name,number,password):
 
-      # docstring removed for simplicity
-
+      
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = number
         self.password = password
+    def save_user(self):
+
+        '''
+        save_user  objects into user_list
+        '''
+
+        User.user_list.append(self)
--- user_test.py
+++ user_test.py
@@ -6,22 +6,22 @@ class Testuser(unittest.TestCase):
 
     def setUp(self):
 
-        """Set up method to run before each test cases.
+        """Set up to run before each test cases.
         """
 
         self.new_user = user("Gabriel","Ndolo","geb888")
 
     def test_init(self):
 
-        """_sutest_init test case to test if the object is initialized properlymmary_
+        """_test_init  test if the object is initialized 
         """
         self.assertEqual(self.new_user.first_name,"Gabriel")
         self.assertEqual(self.new_user.last_name,"Ndolo")
         self.assertEqual(self.new_password,"geb888")
     def test_save_user(self):
         '''
-        test_save_contact test case to test if the contact object is saved into
-         the contact list
+        test_save_user test if the user  is saved into
+         the user list
         '''
         self.new_user.save_user() # saving the new contact
         self.assertEqual(len(user.user_list),1)
