a17d753afaf5eeff58db1658dee80acbd3f36494
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-24 22:45:53 +0300

    ft:added test for the user saved

---
+++ user.py
@@ -0,0 +1,17 @@
+from user import user
+
+class User:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    user_list = [] # Empty contact list
+
+    def __init__(self,first_name,last_name,number,password):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.password = password
--- user_test.py
+++ user_test.py
@@ -1,2 +1,27 @@
 import unittest
 from user import user
+import pyperclip
+
+class Testuser(unittest.TestCase):
+
+    def setUp(self):
+
+        """Set up method to run before each test cases.
+        """
+
+        self.new_user = user("Gabriel","Ndolo","geb888")
+
+    def test_init(self):
+
+        """_sutest_init test case to test if the object is initialized properlymmary_
+        """
+        self.assertEqual(self.new_user.first_name,"Gabriel")
+        self.assertEqual(self.new_user.last_name,"Ndolo")
+        self.assertEqual(self.new_password,"geb888")
+    def test_save_user(self):
+        '''
+        test_save_contact test case to test if the contact object is saved into
+         the contact list
+        '''
+        self.new_user.save_user() # saving the new contact
+        self.assertEqual(len(user.user_list),1)
